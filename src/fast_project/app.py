3f803a68bd3a6bb0e1b84f10efaac5b132886b52
valdsoares360-gif <vald.soares360@gmail.com>
2026-01-27 21:51:34 -0300

    adionando squema

--- src/fast_project/app.py
+++ src/fast_project/app.py
@@ -1,8 +1,31 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from fast_project.squemas import Message
+
+app = FastAPI(title='PROJECT!')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'hello world'}
+
+
+@app.get(
+    '/teste',
+    tags=['testar'],
+    response_class=HTMLResponse,
+)
+def test_html():
+    return """
+    <html>
+        <head>
+            <title>Home</title>
+        </head>
+        <body>
+            <h1>Hello world</h1>
+        </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -13,3 +13,13 @@ def test_read_root_must_return_ok_and_hello_world():
     assert response.status_code == HTTPStatus.OK  # assert(garantir)
 
     assert response.json() == {'message': 'hello world'}
+
+
+def test_teste_must_return_HtmlResponse_hello_world():
+    client = TestClient(app)
+
+    response = client.get('/teste')
+
+    assert response.status_code == HTTPStatus.OK
+
+    assert 'Hello world' in response.text
