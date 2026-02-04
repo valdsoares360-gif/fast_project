e2bcef53473fc8c357eab84f5dfafe5e1b808f94
valdsoares360-gif <vald.soares360@gmail.com>
2026-02-04 16:30:27 -0300

    adicionando a migraçao com o alembic e criando a tabela usuarios.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,42 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import Session
 
 from fast_project.app import app
+from fast_project.models import table_registry
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+        table_registry.metadata.drop_all(engine)
+
+
+@contextmanager
+def db_time_context(*, model, time=datetime.now()):
+    def fake_time_hook(mapper, conection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+
+    event.listen(model, 'before_insert', fake_time_hook)
+    yield time
+    event.remove(model, 'before_insert', fake_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return db_time_context
---
+++ tests/test_db.py
@@ -0,0 +1,23 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from fast_project.models import User
+
+
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='test', email='test123@gmail.com', password='1234'
+        )
+        session.add(new_user)
+        session.commit()
+        user = session.scalar(select(User).where(User.username == 'test'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'test',
+        'email': 'test123@gmail.com',
+        'password': '1234',
+        'created_at': time,
+    }
